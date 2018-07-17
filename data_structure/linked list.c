c22ece0eb36e9b8353cef1394bf759829a3463f3
pawan7798 <pawan.sh.7798@gmail.com>
2018-07-17 17:40:14 +0530

    linked_list

---
+++ data_structure/linked list.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+   int data;
+   struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+//display the list
+void printList()
+{
+  struct node*temp;
+  temp = head;
+  printf("\n[head] =>");
+//start from the beginning
+  while(temp != NULL)
+  {
+     printf(" %d =>",temp->data);
+     temp = temp->next;
+  }
+
+
+  printf(" [null]\n");
+}
+
+//insert link at the first location
+void insert(int data)
+ {
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+
+   //link->key = key;
+   link->data = data;
+
+   //point it to old first node
+   link->next = NULL;
+
+   if(head == NULL)
+   {
+     head = link;
+   }
+
+   if(current == NULL)
+   {
+     current = head;
+   }
+   else
+   {
+     //point first to new node
+     current -> next = link;
+     current = link;
+   }
+
+}
+
+
+int main()
+{
+  insert(10);
+  insert(20);
+  insert(30);
+  insert(1);
+  insert(40);
+  insert(56);
+  printList();
+
+}
