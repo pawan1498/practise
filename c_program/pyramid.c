55854fa17e5505b85976ce023c3b3dfa39e49a8a
pawan7798 <pawan.sh.7798@gmail.com>
2019-01-20 11:13:28 +0530

    min for_loop and comments

--- c_program/pyramid.c
+++ c_program/pyramid.c
@@ -1,23 +1,20 @@
 #include<stdio.h>
 int main()
-{
+{   
+    int star =1; 
     int i ,j,p=5;
-    for(i=0;i<p;i++)
-    {
-        for(int k =p ;k>i;k--)
-        {
-            printf(" ");
+    for(i=0;i<p;i++) // loop is used for next line 
+    {   
+        for(int k =p ;k>i;k--) // used for left empty area
+        {                      // and for making pyramid perfect by moving slightly  
+            printf(" ");      
         }
 
 
-        for(j=i;j>=0;j--)
-        {
-            printf("*");
-        }
-        for(int x=i;x>0;x--)
-        {
-            printf("*");
-        }
-        printf("\n");
-    }
+        for(j=0;j<star;j++)// used for whole pyramid pattern 
+        {                   
+            printf("*");    // means at every iterartion of outer loop star is increased i by 2 
+        }                   
+        star = star+2;     // star is increased by 2  
+        printf("\n");      // initially star is at 1 ,then 3->5->7........
 }
