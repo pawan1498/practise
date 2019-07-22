507350114ce7c0ca10935401b2bf692c2ccf63df
pawan1498 <pawan.sh.7798@gmail.com>
2019-07-22 12:36:31 +0530

    furthur code

--- c_program/string/StringSplosion.c
+++ c_program/string/StringSplosion.c
@@ -1,2 +1,16 @@
 #include<stdio.h>
 #include<string.h>
+int main()
+{
+        char a[100] = "abc";
+        int l = strlen(a);
+        printf("%d\n",l);
+        for( int i =0 ;i<l;i++)
+        {
+                for(int j = 0 ; j<=i;j++)
+                {
+                        printf("%c",a[j]);
+                }
+        }
+}
+
