60cd8e61f0a5a841e53c3d6aa13dab42581ce63f
pawan1498 <pawan.sh.7798@gmail.com>
2020-08-09 22:03:53 +0530

    n queen solution

--- c_program/backtracking/n_queen.c
+++ c_program/backtracking/n_queen.c
@@ -14,79 +14,117 @@ void printsol(int sol[N][N])
 
 int check(int x, int y, int sol[N][N])
 {
-    return ( x >= 0 && x < N && y >= 0 && y < N && sol[x][y]== 1 );
+    return ( x >= 0 && x < N && y >= 0 && y < N );
 }
 
-int solv(int x , int y , int sol[N][N])
+int attackposition(int queen ,int x , int y , int sol[N][N])
 {
-    sol[2][3] = 5;
-    
+   
     for(int i =0 ; i< N ; i++)
     {
         //rows
+       
         if(check(x , i , sol))
         {
-            if(sol[x][i] == 1)
-            sol[x][i] = 0;
-
+            if(sol[x][i] == -1);
+            else
+            {
+                printf("under attack 1");
+                return 0 ;
+            }
         }
         
         //column
        if(check(i,y,sol))
        {
-            if(sol[i][y] == 1 )
-            sol[i][y] = 0;
-
+            if(sol[i][y] == -1 );
+            else
+            {
+                printf("under attack 2");
+                return 0 ;
+            }
        }
 
        //d1+
        if(check(x+i,y+i,sol))
        {
-            if(sol[x+i][y+i] == 1 )
-            sol[x+i][y+i] = 0;
-
+            if(sol[x+i][y+i] == -1 );
+            else
+            {
+                printf("under attack 3");
+                return 0;
+            }
        }
+
        //d1-
        if(check(x-i,y-i,sol))
        {
-            if(sol[x-i][y-i] == 1 )
-            sol[x-i][y-i] = 0;
-
+            if(sol[x-i][y-i] == -1 );
+            else
+            {
+                printf("under attack 4");
+                return 0;
+            }
        }
 
        //d2+
        if(check(x+i,y-i,sol))
        {
-            if(sol[x+i][y-i] == 1 )
-            sol[x+i][y-i] = 0;
+            if(sol[x+i][y-i] == -1 );
+            else
+            {
+                printf("under attack 5");
+                return 0;
+            }
 
        }
 
        //d2-
        if(check(x-i,y+i,sol))
        {
-            if(sol[x-i][y+i] == 1 )
-            sol[x-i][y+i] = 0;
-
+            if(sol[x-i][y+i] == -1 );
+            else
+            {
+                printf("under attack 6");
+                return 0;
+            }
        } 
+    }   
+   return 1;
+}
+
+int place_queen(int queen , int row , int col , int sol[N][N])
+{
+    if( queen == N)
+      return 1 ;
+    queen++;
+    for (int i = 0 ; i<N ;i++)
+    {
+         if(attackposition(queen,row , i , sol ))
+         {
+             sol[row][i] = queen;
+             printsol(sol);
+             if(place_queen(queen , row+1,col,sol) == 1)
+                 return 1;
+             else
+             {
+                 sol[row][i] = -1;
+             }   
+         }
     }
-    printsol(sol);   
 }
 
 int main ()
 { 
     int sol[N][N] ;
-   /* int sol[N][N] = { { 1, 1, 1, 1 },
-                      { 1, 1, 1, 1 },
-                      { 1, 1, 1, 1 },
-                      { 1, 1, 1, 1 } };*/
-    
+   
     for(int i = 0 ; i < N ;i++)
         for (int j = 0 ; j< N ;j++)
         {
-            sol[i][j]=1;
+            sol[i][j]=-1;
         }
-    int x=2 ,y=3; 
-    solv(x,y,sol); 
-   // printsol(sol);
+   int queen=0 ; 
+   int row =0 , col =0 ;  
+   place_queen(queen ,row,col,sol);
+   
 }
