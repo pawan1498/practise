121b6b46c220933e319cb4456defa4aa27176540
pawan1498 <pawan.sh.7798@gmail.com>
2021-01-23 22:09:04 +0530

    nth fibonaci number

---
+++ c_program/recursion/fibbonaci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+int main()
+{
+    int t = 21;
+    printf("%dth fibbonaci number is %d", t, fib(t));
+}
